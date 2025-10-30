3e7874111ae606a740594bcda0cda904894327ec
Ajmat <hunterboyz73770@gmail.com>
2025-10-30 21:40:12 +0530

    Create ErrorBoundary.tsx

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,30 @@
+import { Component, ErrorInfo, ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  state: State = { hasError: false };
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, info: ErrorInfo) {
+    console.error("Error:", error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong...</h1>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
